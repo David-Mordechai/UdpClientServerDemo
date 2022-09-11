30200607bf497a8e7ce134a19045941776f97bcc
David Mordechai <david.mordechai79@gmail.com>
2022-09-11 22:59:51 +0300

    Refactoring Udp Client app, replacing Threads with Tasks

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,16 +4,14 @@ using Server;
 var server = new UdpListener();
 
 // thread for listening incoming messages
-var serverThread = new Thread(() => server.Listen());
-serverThread.Start();
+var serverTask = new Task(() => server.Listen());
+serverTask.Start();
 
 // thread for handling received messages
-var dataHandlerThread = new Thread(() => server.OnDataReceived += OnDataReceived);
-dataHandlerThread.Start();
-
-static void OnDataReceived(object sender, UdpListener.ReceivedDataArgs args)
+var dataHandlerTask = new Task(() => server.OnDataReceived += (_, args) =>
 {
     Console.WriteLine($"Received message from [{args.IpAddress}:{args.Port}] - {Encoding.ASCII.GetString(args.ReceivedBytes)}");
-}
+});
+dataHandlerTask.Start();
 
 Console.ReadKey();
